c9de96bdbbaf1fe951217812a6c70b3f2d1d2f82
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-07-01 15:37:27 -0300

    configuracion del entorno de trabajo

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', TemplateView.as_view(template_name='index.html')),  # Carga el frontend
+
+
+
+    
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name='index.html')) ]
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
